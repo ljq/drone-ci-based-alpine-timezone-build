ba4283473ffc9d61ca4c435720e7d3afb30b6d51
Jack Liu <stackgo@163.com>
2020-08-15 04:56:48 +0800

    Update drone_ci_build.sh

--- drone_ci_build.sh
+++ drone_ci_build.sh
@@ -4,8 +4,21 @@
 echo "drone-server alpine add timezone .\n"
 docker build --target drone-server -t drone/drone:timezone-asia --force-rm .
 
-echo "drone-server alpine add timezone .\n"
+echo "drone-agent alpine add timezone .\n"
 docker build --target drone-agent -t drone/agent:timezone-asia --force-rm .
+
+echo "-------- drone plugins ---------"
+
+echo "drone-scp alpine add timezone .\n"
+docker build --target drone-scp -t appleboy/drone-scp:timezone-asia --force-rm .
+
+echo "drone-ssh alpine add timezone .\n"
+docker build --target drone-ssh -t appleboy/drone-ssh:timezone-asia --force-rm .
+
+echo "----- tools docker timezone ----"
+
+echo "golang:1.14-alpine add timezone .\n"
+docker build --target golang-tz -t appleboy/golang:timezone-asia --force-rm .
 
 echo "Dockerfile Multi Stage Finished. \n"
 
